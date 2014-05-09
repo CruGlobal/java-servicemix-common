58f445d17ab2b4e0180dbdcc5ef04f86d505d72b
Daniel Frett <daniel.frett@ccci.org>
2014-05-09 15:18:19 -0400

    make it possible to specify the access_token as a query parameter

--- core/src/main/java/org/ccci/gto/servicemix/common/jaxrs/api/AccessTokenBasedApi.java
+++ core/src/main/java/org/ccci/gto/servicemix/common/jaxrs/api/AccessTokenBasedApi.java
@@ -1,5 +1,7 @@
 package org.ccci.gto.servicemix.common.jaxrs.api;
 
+import static org.ccci.gto.servicemix.common.jaxrs.api.Constants.PARAM_ACCESS_TOKEN;
+
 import com.google.common.base.CharMatcher;
 import org.apache.cxf.jaxrs.ext.MessageContext;
 
@@ -23,10 +25,17 @@ public abstract class AccessTokenBasedApi extends AbstractApi {
             }
         }
 
-        // look up the access token
+        // look up the access token in the following preference order:
+        // 1. path parameter
+        // 2. query parameter
+        // 3. Authorization header
         String accessToken = null;
         if (uri != null) {
             accessToken = this.getAccessTokenFromPath(uri);
+
+            if (accessToken == null) {
+                accessToken = this.getAccessTokenFromQuery(uri);
+            }
         }
         if (accessToken == null && headers != null) {
             accessToken = this.getAccessTokenFromAuthorizationHeader(headers);
@@ -36,12 +45,21 @@ public abstract class AccessTokenBasedApi extends AbstractApi {
 
     protected abstract String getAccessTokenPathParam();
 
-    protected final String getAccessTokenFromPath(UriInfo uri) {
+    private String getAccessTokenFromPath(UriInfo uri) {
         final String accessToken = uri.getPathParameters().getFirst(getAccessTokenPathParam());
         return accessToken != null ? MATCHER_FORWARD_SLASH.trimFrom(accessToken) : null;
     }
 
-    protected final String getAccessTokenFromAuthorizationHeader(final HttpHeaders headers) {
+    protected String getAccessTokenQueryParam() {
+        return PARAM_ACCESS_TOKEN;
+    }
+
+    private String getAccessTokenFromQuery(UriInfo uri) {
+        final String accessToken = uri.getQueryParameters().getFirst(getAccessTokenQueryParam());
+        return accessToken != null ? accessToken : null;
+    }
+
+    private String getAccessTokenFromAuthorizationHeader(final HttpHeaders headers) {
         // check for Bearer Authorization header
         final List<String> values = headers.getRequestHeader(HttpHeaders.AUTHORIZATION);
         if (values != null) {
--- core/src/main/java/org/ccci/gto/servicemix/common/jaxrs/api/Constants.java
+++ core/src/main/java/org/ccci/gto/servicemix/common/jaxrs/api/Constants.java
@@ -6,8 +6,12 @@ public final class Constants {
     public static final String PATH_SESSION = "{sessionId:[0-9a-f]+}";
     public static final String PATH_OPTIONAL_SESSION = "{sessionId:([0-9a-f]+/)?}";
 
+    // path params
     public static final String PARAM_API_KEY = "apiKey";
     public static final String PARAM_SESSION = "sessionId";
     public static final String PARAM_TICKET = "ticket";
     public static final String PARAM_GUEST = "guest";
+
+    // query params
+    public static final String PARAM_ACCESS_TOKEN = "access_token";
 }
